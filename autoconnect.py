508f38a11b37440963f54f497ea8a9413cfa9299
Chris Byrd <christopher.byrd2013@gmail.com>
2017-10-12 10:09:20 -0500

    Added syslog interaction to the Python script.

--- autoconnect.py
+++ autoconnect.py
@@ -1,38 +1,44 @@
 #!/usr/bin/env python3.5
 import os
+import syslog
 import time
 import mechanicalsoup
 
-print("autoconnect.py: Modules imported")
+
+def log(message):
+    syslog.syslog(message)
+
 
 def checkconnection():
 	try:
 		result = os.system("ping -c 1 google.com")
 		if result == 0:
-			print("autoconnect.py: Ping to Google successful")
+			log("autoconnect.py: Ping to Google successful")
 			return True
-		else:
-			print("autoconnect.py: Ping to Google failed")
-			return False
+		log("autoconnect.py: Ping to Google failed")
 	except:
-		print("autoconnect.py: Ping command failed")
-		return False
+		log("autoconnect.py: Ping command failed")
+    return False
+
 
 def getconnected():
 	try:
 		browser = mechanicalsoup.StatefulBrowser()
 		browser.open("http://www.capitol.state.tx.us")
 		browser.follow_link("continue")
-		print("autoconnect.py: Browser run complete")
+		log("autoconnect.py: Browser run complete")
 	except Exception as e:
-		print("autoconnect.py: Browser run failed")
-		print(e)
+		log("autoconnect.py: Browser run failed")
+		log(e)
 		return False
 
+
 def main():
+	log("autoconnect.py: Modules imported")
 	connected = checkconnection()
 	if not connected:
 		getconnected()
+
 
 if __name__ == "__main__":
 	main()
